e639520b6a609c34f7eb4a513fea21da17db08d8
mouaammou <mouadamassine@gmail.com>
2024-05-17 20:47:12 +0100

    update the model, and add form validation

--- backend/project/authentication/models.py
+++ backend/project/authentication/models.py
@@ -8,12 +8,19 @@ from django.contrib.auth.hashers import make_password
 class CustomUser(AbstractUser):
 	username = models.CharField(max_length=255, unique=True)
 	email = models.EmailField(unique=True)
+	first_name = models.CharField(max_length=255, blank=False)
+	last_name = models.CharField(max_length=255, blank=False)
+	password = models.CharField(max_length=255, blank=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
-	def save(self, *args, **kwargs):
-		# Hash the password before saving
-		if self.password:
-			self.password = make_password(self.password)
-		super().save(*args, **kwargs)
+	# def save(self, *args, **kwargs):
+	# 	# Hash the password before saving
+	# 	if self.password:
+	# 		self.password = make_password(self.password)
+	# 	super().save(*args, **kwargs)
+
 	def __str__(self):
 		return self.username
 	
--- frontend/src/app/auth/pages/signup.jsx
+++ frontend/src/app/auth/pages/signup.jsx
@@ -4,94 +4,153 @@ import { useState } from "react";
 import axios from "axios";
 
 function Signup() {
-   const [username, setUsername] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const [confirmPassword, setConfirmPassword] = useState("");
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		username: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
 
-   const [invalidInput, setInvalidInput] = useState(false);
-   const [errorMessage, setErrorMessage] = useState("");
+	const [errors, setErrors] = useState({});
 
-   const postData = {
-      username: username,
-      email: email,
-      password: password,
-   };
+	const validateFrom = () => {
+		const errors = {};
+		setErrors(errors);
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      if (password !== confirmPassword) {
-         setInvalidInput(true);
-         return;
-      }
-      setInvalidInput(false);
-      setErrorMessage("");
+		//validate username
+		if (!formData.username) {
+			errors.username = "Username is required";
+		} else if (
+			formData.username.length < 5 ||
+			formData.username.length > 20
+		) {
+			errors.username = "Username must be between 5 and 20 characters";
+		} else if (!/^[a-zA-Z]+$/.test(formData.username)) {
+			//must username contain only letters;
+			errors.username = "Username must contain only letters";
+		}
 
-      //make a post request to the server
-      axios
-         .post("http://localhost:8000/signup/", postData)
-         .then((res) => {
-            console.log("data==> ", res.data);
-         })
-         .catch((error) => {
-            console.log("error ==> ", error);
-            console.log("username: ", error.response.data.username);
-            error.response.data.username;
-            error.response.data.email;
-            error.response.data.password;
+		//validate email
+		if (!formData.email) {
+			errors.email = "Email is required";
+		} else if (
+			!/\S+@\S+\.\S+/.test(formData.email) ||
+			formData.email.length < 10
+		) {
+			//this regex means that email must contain @ and .
+			errors.email = "Email is invalid";
+		}
 
-            setErrorMessage(
-               <>
-                  <p>{error.response.data.username}</p>
-                  <p>{error.response.data.email}</p>
-                  <p>{error.response.data.password}</p>
-               </>
-            );
-         });
-   };
+		//validate password
+		if (!formData.password) {
+			errors.password = "Password is required";
+		} else if (formData.password.length < 8) {
+			errors.password = "Password must be at least 8 characters";
+		} else if (
+			!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(
+				formData.password
+			)
+		) {
+			errors.password =
+				"Password must contain at least one uppercase letter, one lowercase letter and one number";
+		}
 
-   return (
-      <div className="signup">
-         <h1>sign up</h1>
-         <form onSubmit={handleSubmit} method="POST">
-            <input
-               type="text"
-               placeholder="username"
-               name="username"
-               required
-               onChange={(e) => {
-                  setUsername(e.target.value);
-                  setErrorMessage("");
-               }}
-            />
-            <input
-               type="email"
-               placeholder="email"
-               name="email"
-               required
-               onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-               type="password"
-               placeholder="password"
-               name="password"
-               required
-               onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-               type="password"
-               placeholder="confirm password"
-               name="confirmPassword"
-               required
-               onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button type="submit">submit</button>
-            <br />
-            {invalidInput && <p>Passwords do not match</p>}
-            {errorMessage}
-         </form>
-      </div>
-   );
+		//validate confirmPassword
+		if (!formData.confirmPassword) {
+			errors.confirmPassword = "Confirm password is required";
+		} else if (formData.password !== formData.confirmPassword) {
+			errors.confirmPassword = "Passwords do not match";
+		}
+
+		setErrors(errors);
+	};
+
+	const PostData = async (e) => {
+		e.preventDefault();
+		validateFrom();
+		const errors = {};
+		setErrors(errors);
+		if (Object.keys(errors).length > 0) {
+			return;
+		}
+
+		const { confirmPassword, ...dataToSend } = formData;
+		//make a post request to the server
+		await axios
+			.post(
+				"/api/signup/",
+				dataToSend,
+				(headers = {
+					"Content-Type": "application/json",
+				})
+			)
+			.then((res) => {
+				//if the request is successful
+				console.log("data==> ", res.data);
+			})
+			.catch((error) => {
+				//if the request is not successful
+				console.log("error ==> ", error);
+				console.log("username: ", error.response.data.username);
+				error.response.data.username;
+				error.response.data.email;
+				error.response.data.password;
+
+				setErrors({
+					username: error.response.data.username,
+					email: error.response.data.email,
+					password: error.response.data.password,
+				});
+			});
+	};
+
+	return (
+		<div className="signup">
+			<h1>sign up</h1>
+			<form onSubmit={PostData} method="POST">
+				<input
+					type="text"
+					placeholder="username"
+					name="username"
+					required
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="email"
+					placeholder="email"
+					name="email"
+					required
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="password"
+					name="password"
+					required
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="confirm password"
+					name="confirmPassword"
+					required
+					onChange={(e) => setConfirmPassword(e.target.value)}
+				/>
+				<button type="submit">submit</button>
+				<br />
+				{errors && (
+					<div>
+						{errors.username && <p>{errors.username}</p>}
+						{errors.email && <p>{errors.email}</p>}
+						{errors.password && <p>{errors.password}</p>}
+						{errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+					</div>
+				)}
+			</form>
+		</div>
+	);
 }
 
 export default Signup;
