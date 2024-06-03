d06a1b457edd822bc08c8ca27d185cc4a99a0298
mouaammou <mouadamassine@gmail.com>
2024-06-03 23:38:36 +0100

    backend is merged, frontend not yet

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -5,9 +5,9 @@ from .views import OAuth42Login, OAuth42Callback
 from . import views
 
 urlpatterns = [
-	path("signup",views.SignUp, name="singup"),
-	path("login",views.Login, name="login"),
-	path("logout",views.Logout, name="logout"),
+	path("signup/",views.SignUp, name="singup"),
+	path("login/",views.Login, name="login"),
+	path("logout/",views.Logout, name="logout"),
 	path("",views.default, name="default"),
 
 	path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"L88Erptv6EgZ2dFAU1F2yKuuMSJQXifk3Swfpn5oGig=\"\n}"
