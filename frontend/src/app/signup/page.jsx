5957d8530a28c6621c205ee1f5476c1e4042569d
Mohamed Ouaammou <moouaamm@e3r10p17.1337.ma>
2024-06-04 22:12:26 +0100

    singup added

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -1,33 +1,31 @@
-import Link from "next/link";
 import "./style.css"
 
-const SignUp = () => {
+const LogIn = () => {
 	
 	return (
 		<div className="main-container">
             <div className="main-login">
-                <p>Join</p>
+                <p>Sing Up</p>
+                <div className="first-last-names">
+                    <input type="text" placeholder="First name"/>
+                    <input type="text" placeholder="Last name"/>
+                </div>
+                <input type="text" placeholder="Username"/>
                 <input type="text" placeholder="Enter Your Email"/>
                 <input type="text" placeholder="Enter Your Password"/>
-                <button>Login</button>
+                <input type="text" placeholder="Confirm Password"/>
+                <button>Register</button>
                 <img src="login-with.svg" alt="login-with" className="sign-with"/>
                 <div className="logos">
                     <img src="g3.svg" alt=""  className="_42-logo" />
                     <img src="google-icon.png" alt="" className="google-logo" />
                 </div>
-                <div className="forgot-password">
-                    <a href="">Forgot your password?</a>
-                    <div className="have-no-account">
-                        Don't have an account? 
-                        <Link rel="stylesheet" href="/signup">Sign up</Link>
-                    </div>
-                </div>
             </div>
             <div className="side-image">
-                <img className="sign-with" src="join.svg" alt="welcome"/>
+                <img className="sign-with" src="login-pane.svg" alt="welcome"/>
             </div>
         </div>
 	)
 }
 
-export default SignUp;
+export default LogIn;
