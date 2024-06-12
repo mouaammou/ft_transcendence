af3d4e1e4ae6a3ddab02d723b57bd186aa2d47a4
mouaammou <mouadamassine@gmail.com>
2024-06-12 02:29:12 +0100

    using media to store the images of the user

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -8,20 +8,19 @@
 
 # test_kargs(2,3,a="slala")
 
-def decorator(func):
-	def wrapper(request, *args, **kwargs):
-		print("Before function")
-		# resutl = func(*args, **kwargs)
-		# print(f"Result: {resutl}")
-		for key, value in kwargs.items():
-			print(f"{key} = {value}")
-		print(f"request: {request}")
-		print("After function")
-	return wrapper
+# def decorator(func):
+# 	def wrapper(request, *args, **kwargs):
+# 		print("Before function")
+# 		# resutl = func(*args, **kwargs)
+# 		# print(f"Result: {resutl}")
+# 		for key, value in kwargs.items():
+# 			print(f"{key} = {value}")
+# 		print(f"request: {request}")
+# 		print("After function")
+# 	return wrapper
 
-@decorator
-def test_kargs(request):
-	return True
+# @decorator
+# def test_kargs(request):
+# 	return True
 
-test_kargs("this is the requsest asi hemad")
-
+# test_kargs("this is the requsest asi hemad")
