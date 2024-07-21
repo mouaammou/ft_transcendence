8aed70ee0223dfcd0b58aec26643ada727465128
mohammedelkouche <mohammed0elkouche@gmail.com>
2024-07-21 20:41:08 +0100

    impliment mobile navbare

--- frontend/src/app/chat/page.jsx
+++ frontend/src/app/chat/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import '@/Styles/chat/chat.css'
-import '@/Styles/sidebar/sidebarMobil.css'
+import '@/Styles/style-sidebar/sidebarMobil.css'
 
 import Msgchat from  '@/components/chat/msg/msg_chat'
 import Listchat from '@/components/chat/list/list_chat'
@@ -11,9 +11,9 @@ const Chat = () => {
 				<div className="main-chat">
 					<Listchat/>
 					<Msgchat/>
-					{/* <div className='chat-mini-sidebare'>
+					{/* <div className='chat-mini-sidebare'> */}
 						<Sidebar/>
-					</div> */}
+					{/* </div> */}
 				</div>
 	 );
 }
