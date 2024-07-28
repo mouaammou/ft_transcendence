b5013daa0423c919617a43b7a4c1fc9e94f712a7
mouaammou <mouadamassine@gmail.com>
2024-07-28 16:49:04 +0100

    midlleware is activate in nextjs

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -4,19 +4,17 @@ export function middleware(request) {
 	const access_token = request.cookies.get("access_token");
 	const refresh_token = request.cookies.get("refresh_token");
 
-	console.log("access token", access_token);
-	console.log("refresh token", refresh_token);
+	const isAuthPage =
+		request.url.includes("/auth/login") ||
+		request.url.includes("/auth/signup");
 
-	if (!access_token || !refresh_token)
-		return NextResponse.redirect(new URL("/auth/login", request.url));
-
-	if (
-		access_token &&
-		refresh_token &&
-		(request.url == "http://localhost:3000/auth/login" ||
-			request.url == "http://localhost:3000/auth/signup")
-	)
+	if (access_token && refresh_token && isAuthPage)
 		return NextResponse.redirect(new URL("/", request.url));
+	else if ((!access_token || !refresh_token) && isAuthPage)
+		NextResponse.next()
+	else if ((!access_token || !refresh_token) && !isAuthPage)
+		return NextResponse.redirect(new URL("/auth/login", request.url));
+	
 	return NextResponse.next()
 }
 
