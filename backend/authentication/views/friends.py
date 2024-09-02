32d57e1ac58144b9299c2dbc8d1e18a34fba600d
Mouad Ouaammou <mouaammo@e3r8p1.1337.ma>
2024-09-02 10:52:25 +0100

    handle accept, and block views, get allfriends return only the accepted and blocked friends

--- backend/authentication/views/friends.py
+++ backend/authentication/views/friends.py
@@ -6,10 +6,14 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework import status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 class FriendshipListCreateView(generics.ListCreateAPIView):
-    queryset = Friendship.objects.all()
     serializer_class = FriendsSerializer
+
+    def get_queryset(self):
+        queryset = Friendship.objects.filter(status__in=['blocked', 'accepted'])
+        return queryset
 
 class FriendshipRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Friendship.objects.all()
@@ -22,7 +26,7 @@ class AcceptFriendshipView(generics.GenericAPIView):
     def post(self, request, pk):
         friendship = get_object_or_404(Friendship, pk=pk)
 
-        if friendship.status == 'pending':
+        if friendship.status in ('accepted', 'pending') :
             try:
                 friendship.accept()
                 return Response({"message": "Friendship request accepted."}, status=status.HTTP_200_OK)
@@ -44,6 +48,8 @@ class BlockFriendshipView(generics.GenericAPIView):
                 return Response({'status': 'friendship blocked'}, status=status.HTTP_200_OK)
             except ValidationError as e:
                 return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({"error": "This request has already been processed."}, status=status.HTTP_400_BAD_REQUEST)
+        elif friendship.status == 'blocked':
+            return Response({"error": "This request has already been blocked."}, status=status.HTTP_400_BAD_REQUEST)
 
+#get all friends: blocked and accepted
+    
