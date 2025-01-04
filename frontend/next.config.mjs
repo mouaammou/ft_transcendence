eceed7ab65cd2f106384139410448ce724b85b90
mouaammou <mouadamassine@gmail.com>
2025-01-04 17:32:50 +0100

    done

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = true; // Just change this to true when you want production settings
-
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    if (isProd) {
-      config.module.rules.push({
-        test: /\.css$/i,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-        ],
-      });
-    }
-    return config;
-  },
   async rewrites() {
     return [
       {
