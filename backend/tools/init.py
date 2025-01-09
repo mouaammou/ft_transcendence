99b41ccbbb1d6ec4241e0db41265b23a836a8eed
mouaammou <mouadamassine@gmail.com>
2025-01-09 05:57:02 +0100

    this commit is for save

--- backend/tools/init.py
+++ backend/tools/init.py
@@ -68,8 +68,8 @@ def initialize_django():
         sys.exit(1)
 
 def run_server():
-    # os.system("daphne -b 0.0.0.0 -p 8000 core.asgi:application")
-    os.system("python3 manage.py runserver 0.0.0.0:8000")
+    os.system("daphne -b 0.0.0.0 -p 8000 core.asgi:application")
+    # os.system("python3 manage.py runserver 0.0.0.0:8000")
 
 # Create a default superuser if none exists
 
